d6ca35170f1e93da4d3be0409e471095df376b0d
Maxime LUCE <maxime@touchify.co>
2018-04-30 04:57:28 +0200

    Remove dist from repo

--- _definitions.d.ts
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="typings/index.d.ts" />
-/// <reference path="src/base.d.ts" />
-
-declare var define: any;
-declare var require: any;
-declare var requirejs: any;
-declare var module: any;
-declare var exports: any;
